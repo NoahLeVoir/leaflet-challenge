509ae3656b879795cfdcd91eb45c35ecf1d0bcb2
Noah LeVoir <noahlevoir@Noahs-MacBook-Pro.local>
2021-05-12 19:47:50 -0500

    Generate initial map element with random zoom and center values

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,6 +6,19 @@ console.log("Geo JSON data loaded");
 console.log(geojsonURL);
 
 // Draw initial map
+function drawMap(earthquakes) {
+    
+    // Set up initial Map
+    var earthquakeMap = L.map("mapid", {
+        center: [40.7, -73.95],
+        zoom: 5
+    });
+
+}
+
+// Call the drawMap function
+drawMap();
+
 
 
 // Add Magnitude Markers to the map
