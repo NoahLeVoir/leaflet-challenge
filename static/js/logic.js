eb7fb2d93e32ae0f07258b77e2dd8312ce910cc9
Noah LeVoir <noahlevoir@Noahs-MacBook-Pro.local>
2021-05-12 19:38:24 -0500

    Set up url to be able to pull in geogjson info

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,9 @@
 console.log("logic.js is connected to the index.html");
 
 // Connect to USGS GeoJSON Feed API
-
+var geojsonURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+console.log("Geo JSON data loaded");
+console.log(geojsonURL);
 
 // Draw initial map
 
