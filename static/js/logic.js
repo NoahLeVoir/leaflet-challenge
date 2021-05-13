70d907cbcafb4c8330f83bac24110653b70b354d
Noah LeVoir <noahlevoir@Noahs-MacBook-Pro.local>
2021-05-12 20:13:38 -0500

    Create title layer and overlay layer for earthquakeMap

--- static/js/logic.js
+++ static/js/logic.js
@@ -8,10 +8,31 @@ console.log(geojsonURL);
 // Draw initial map
 function drawMap(earthquakes) {
     
+    // Create the tile layer to be the background the map
+    // Will use mapbox "lightmap" as the title layer
+    var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "outdoors-v11",
+        accessToken: API_KEY
+    });
+
+    // Create a baseMaps object to hold the outdoorsMap layer
+    var baseMaps = {
+        "Geography Map": outdoorsMap
+    };
+
+    // overlayMaps to hold the earthquake layer (NEED TO CREATE STILL)
+    var overlayMaps = {
+        "Earthquakes": earthquakes
+    };
+  
     // Set up initial Map
+    // Add base and overlay layers to map
     var earthquakeMap = L.map("mapid", {
         center: [40.7, -73.95],
-        zoom: 5
+        zoom: 5,
+        layers: [outdoorsMap, earthquakes]
     });
 
 }
