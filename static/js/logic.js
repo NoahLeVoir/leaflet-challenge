964409273716bc8499c4be66f81b44070d0cba00
Noah LeVoir <noahlevoir@Noahs-MacBook-Pro.local>
2021-05-12 21:44:55 -0500

    Added a lightmap layer and layer controls to the map

--- static/js/logic.js
+++ static/js/logic.js
@@ -18,9 +18,17 @@ function drawMap(earthquakes) {
         accessToken: API_KEY
     });
 
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "light-v10",
+        accessToken: API_KEY
+    });
+
     // Create a baseMaps object to hold the outdoorsMap layer
     var baseMaps = {
-        "Geography Map": outdoorsMap
+        "Geography Map": outdoorsMap,
+        "Light Map": lightmap
     };
 
     // overlayMaps to hold the earthquake layer
@@ -35,6 +43,12 @@ function drawMap(earthquakes) {
         zoom: 5,
         layers: [outdoorsMap, earthquakes]
     });
+
+    // Pass our map layers into our layer control
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(earthquakeMap);
 
 }
 
